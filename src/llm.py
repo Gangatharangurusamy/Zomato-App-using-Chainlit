cdf079191d133eb35af0ca505e19f391412413de
gangatharan01 <gangatharan0401@gmail.com>
2025-01-05 17:06:29 +0530

    Added llm.py

--- src/llm.py
+++ src/llm.py
@@ -3,38 +3,30 @@ from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-# from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
-# from src.config import instruction
+from src.config import instruction
 
 load_dotenv()
 
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
-
-def ask_bot(message):
-    llm = ChatGoogleGenerativeAI(model="gemini-pro")
-    response = llm.invoke(message)
-    return response.content
-# def ask_bot(user_message,instruction):
+def ask_bot(user_message,instruction=instruction):
     
-#     model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
+    model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
     
     
-#     respones=model(
-#     [
-#         SystemMessage(content=instruction),
-#         HumanMessage(content=user_message),
-#     ]
-# )
+    respones=model(
+    [
+        SystemMessage(content=instruction),
+        HumanMessage(content=user_message),
+    ]
+)
     
-#     return respones.content
+    return respones.content
 
 if __name__=="__main__":
-    print("testing")
-    message = ask_bot("What is meaning of Large Language Model?")
-    # user_message = "hi how are you?"
-    # respones=ask_bot(user_message)
-    print(message)
-    
+    user_message = "hi how are you?"
+    respones=ask_bot(user_message)
+    print(respones)
